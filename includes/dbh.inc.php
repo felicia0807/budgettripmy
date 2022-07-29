aef055d89076f8faf968346f8645b61db2fdedd9
felicia0807 <61722151+felicia0807@users.noreply.github.com>
2022-07-29 23:48:48 +0800

    Add files via upload

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -5,33 +5,24 @@
 //$dbPassword="";
 //$dbName="travel_db";
 
-$serverName ="remotemysql.com";
-$dBUsername="mLTIwFvbID";
-$dbPassword="VnHGh97F7J";
-$dbName="mLTIwFvbID";
+//remote db
+//$serverName ="remotemysql.com";
+//$dBUsername="sG1fqUOwsd";
+//$dbPassword="veRzq22HZT";
+//$dbName="sG1fqUOwsd";
+
+//heroku db
+$serverName ="us-cdbr-east-06.cleardb.net";
+$dBUsername="b8c38ee8cdeebe";
+$dbPassword="84458ac0";
+$dbName="heroku_295107ec2eabab3";
 
 $conn =mysqli_connect($serverName,$dBUsername,$dbPassword,$dbName);
 
 if(!$conn) {
     die("Connection failed".mysqli_connect_error());
 }
-
-$create_db_sql    = "CREATE DATABASE IF NOT EXISTS $dbName";
-$create_table_sql = "CREATE TABLE IF NOT EXISTS bookings (
-    loc_id int UNSIGNED PRIMARY KEY,
-    date varchar(100) NOT NULL,
-    qty int NOT NULL,
-    amount decimal(10,2) NOT NULL
-)";
-
-mysqli_query($conn, $create_db_sql); // run query to create database
-$conn = new mysqli($serverName,$dBUsername,$dbPassword,$dbName); // create new connection and connect to the ewly created database
-
-if(!$conn){
-    die(mysqli_error($conn));
-}
-
-mysqli_query($conn, $create_table_sql); // run query to create tale
+    
 ?> 
 
 
